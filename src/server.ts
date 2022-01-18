541ea52bd60c75d1d97e27c2945b1714b527523c
Omair Muhi <omairmuhi@icloud.com>
2022-01-18 17:54:27 -0500

    Subject: Changes for deployment Problem Solution Note

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+require("dotenv").config();
 
 (async () => {
   // Init the Express application
